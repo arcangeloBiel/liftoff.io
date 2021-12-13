fe45ef956c88d0e545000a2ced4ae8bd8fc38561
joao ribeiro <joao.ribeiro@havan.com.br>
2021-12-13 05:57:14 -0300

    Create migrations + entities TAGS

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,28 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import "reflect-metadata";
 import "./database";
 import { router } from './routes';
+import "express-async-errors"
+
 
 const app = express();
-app.use(express.json())
 
- app.use(router);
+app.use(express.json())
+app.use(router);
+
+/** 
+ * middleware são interceptores
+*/
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => { 
+
+    if(err instanceof Error) {
+       return res.status(400).json({error: err.message});
+    }
+
+    return res.status(500).json({status: "error", message: "Internal Server Error"});
+
+});
+
 
 //https://localhost:300
 app.listen(3000, () => { console.log(" Server is running NLW") });
